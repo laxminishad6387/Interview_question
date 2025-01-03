e4d04dc84db9abd39775dc50b28abc1a732a3434
laxmi nishad <laxminishad6387@gmail.com>
2025-01-03 13:47:38 +0530

    Time: 0 ms (100%), Space: 40.3 MB (84.05%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,26 +1,14 @@
+// ask in microsft company
+
 class Solution {
     public int findTheWinner(int n, int k) {
-        Deque<Integer> q=new LinkedList<Integer>();
-        // push element 1 to n in deque
-       int i=1;
-        while(i<=n)
-        {
-            q.addLast(i);
-            i++;
-        }
-        // check deque size is grater than 1 or not
-        while(q.size()!=1)
-        {
-            int j=1;
-            while(j<k)
-            {
-                int temp=q.removeFirst();
-                q.addLast(temp);
-                j++;
-            }
-            q.removeFirst();
-        }
-        return q.removeFirst();
-
-    }
+        // this type of logic is use for circular game related problem solving
+     int i=1;
+     for(int j=2; j<=n; j++)
+     {
+        i+=k;
+        i=(i-1)%j+1;
+     }
+     return i;
+ }
 }
