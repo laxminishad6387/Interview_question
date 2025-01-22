accc8e20a70f24ab1dca013f61720f1e79ce1565
laxmi nishad <laxminishad6387@gmail.com>
2025-01-23 05:21:42 +0530

    Time: 7 ms (17.46%), Space: 45.4 MB (9.74%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,44 +1,38 @@
+// backtracking pproblem question(it's required all possible outcomes)
+
 class Solution {
-    public boolean isSafe(ArrayList<String> board ,int row,int col,int n)
-    {
-        // check horizontal 
-        for(int i=0; i<n; i++)
-        {
-            if(board.get(row).charAt(i)=='Q')
-            return false;
+    public boolean isSafe(ArrayList<String> board, int row, int col, int n) {
+        // check horizontal
+        for (int i = 0; i < n; i++) {
+            if (board.get(row).charAt(i) == 'Q')
+                return false;
         }
-         // check vertical 
-        for(int i=0; i<n; i++)
-        {
-            if(board.get(i).charAt(col)=='Q')
-            return false;
+        // check vertical
+        for (int i = 0; i < n; i++) {
+            if (board.get(i).charAt(col) == 'Q')
+                return false;
         }
-         // check left side diagonal 
-        for(int i=row , j=col; i>=0 && j>=0 ; i--,j--)
-        {
-            if(board.get(i).charAt(j)=='Q')
-            return false;
+        // check left side diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board.get(i).charAt(j) == 'Q')
+                return false;
         }
-         // check right side diagonal 
-        for(int i=row , j=col; i>=0 && j<n ; i--, j++)
-        {
-            if(board.get(i).charAt(j)=='Q')
-            return false;
+        // check right side diagonal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board.get(i).charAt(j) == 'Q')
+                return false;
         }
         return true;
     }
-    public void backtrack( List<List<String>>result,ArrayList<String> board ,int row, int n)
-    {
-        if(row==n)
-        {
+
+    public void backtrack(List<List<String>> result, ArrayList<String> board, int row, int n) {
+        if (row == n) {
             result.add(new ArrayList<>(board));
             return;
         }
-        for(int i=0; i<n; i++)
-        {
-            if(isSafe(board,row,i,n))
-            {
-               char[] rowArray = board.get(row).toCharArray();
+        for (int i = 0; i < n; i++) {
+            if (isSafe(board, row, i, n)) {
+                char[] rowArray = board.get(row).toCharArray();
                 rowArray[i] = 'Q';
                 board.set(row, new String(rowArray));
 
@@ -51,16 +45,17 @@ class Solution {
             }
         }
     }
+
     public List<List<String>> solveNQueens(int n) {
-          
+
         ArrayList<String> board = new ArrayList<>();
-        String row = ".".repeat(n); 
-       for (int i = 0; i < n; i++) {
-           board.add(row); 
+        String row = ".".repeat(n);
+        for (int i = 0; i < n; i++) {
+            board.add(row);
         }
-       
-        List<List<String>> result=new ArrayList<>();
-        backtrack(result,board,0,n);
+
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, board, 0, n);
         return result;
     }
 }
