c22ee41f559af546b99c044b2f60ccb9663eee8d
laxmi nishad <laxminishad6387@gmail.com>
2025-01-24 14:39:56 +0530

    Time: 16 ms (17.53%), Space: 44.8 MB (43.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,13 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-         s = s.replaceAll("[^a-zA-Z0-9]", "");//remove all non-alphanumeric character from the string
-          s=s.toLowerCase();
-          int j=s.length()-1;
+    public boolean isPalindrome(String s) {  
+         //palindrome: if we read from left side or right side . we got  a same meaning or character from the both side
+          s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
           for(int i=0; i<s.length()/2; i++)
           {
-            if(s.charAt(i)!=s.charAt(j))
-            return false;
-            j--;
-
+            if(s.charAt(i)!=s.charAt(s.length()-1-i))
+               return false;
           }
-        
-        return true;
+          return true;
+
     }
 }
