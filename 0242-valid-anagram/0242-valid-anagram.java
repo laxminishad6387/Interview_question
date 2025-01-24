15226a15ae3d93afb586be38ed19f723bb34082d
laxmi nishad <laxminishad6387@gmail.com>
2025-01-24 14:47:13 +0530

    Time: 7 ms (37.79%), Space: 44.7 MB (44.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,20 @@
-// solve using String approch
 class Solution {
-    public String helper(String s)
-    {
-         char s1[]=s.toCharArray();
-         Arrays.sort(s1);
-         return new String(s1);
-    }
-    public boolean isAnagram(String s, String p) {
-      String s1= helper(s);
-      String p1= helper(p);
-        if(s1.length()!=p1.length())
-        return false;
-        for(int i=0; i<s1.length(); i++)
-        {
-            if(s1.charAt(i)!=p1.charAt(i))
-              return false;
-        }
+    public boolean isAnagram(String s, String t) {
+        s=s.toLowerCase();
+        t=t.toLowerCase();
+       char str1[]=s.toCharArray();
+       char str2[]=t.toCharArray();
+       Arrays.sort(str1);
+       Arrays.sort(str2);
+       if(str1.length!=str2.length)
+         return false;
+         else{
+       for(int i=0; i<str1.length; i++)
+       {
+        if(str1[i]!=str2[i])
+          return false;
+       }
+         }
         return true;
 
     }
