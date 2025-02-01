f8406edc20a469ddfc0981e57d8ec0833c26d702
laxmi nishad <laxminishad6387@gmail.com>
2025-02-01 19:03:00 +0530

    Time: 2 ms (97.35%), Space: 44.9 MB (15.31%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,51 +1,25 @@
-// apply depth first search
 
 class Solution {
-    public boolean check(TreeNode root, TreeNode subroot) {
-        Queue<TreeNode> q = new LinkedList<>();
-        Queue<TreeNode> p = new LinkedList<>();
-        q.add(root);
-        p.add(subroot);
-
-        while (!q.isEmpty() && !p.isEmpty()) {
-            TreeNode ele1 = q.remove();
-            TreeNode ele2 = p.remove();
-
-            if (ele1 == null || ele2 == null || ele1.val != ele2.val) {
-                return false;
-            }
-
-            if (ele1.left != null || ele2.left != null) {
-                if (ele1.left == null || ele2.left == null) return false;
-                q.add(ele1.left);
-                p.add(ele2.left);
-            }
-
-            if (ele1.right != null || ele2.right != null) {
-                if (ele1.right == null || ele2.right == null) return false;
-                q.add(ele1.right);
-                p.add(ele2.right);
-            }
-        }
-        return q.isEmpty() && p.isEmpty();
+    boolean check(TreeNode root, TreeNode subRoot)
+    {
+        if(root==null && subRoot==null)
+        return true;
+        else if(root==null ||subRoot==null)
+          return false;
+          else if(root.val!=subRoot.val)
+          return false;
+          return check(root.left,subRoot.left) && check(root.right,subRoot.right);
     }
-
-    public boolean isSubtree(TreeNode root, TreeNode subroot) {
-        if (root == null) return false;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while (!q.isEmpty()) {
-            TreeNode ele = q.remove();
-
-            if (ele.val == subroot.val && check(ele, subroot)) {
-                return true;
-            }
-
-            if (ele.left != null) q.add(ele.left);
-            if (ele.right != null) q.add(ele.right);
-        }
-        return false;
+    boolean findRoot(TreeNode root, TreeNode subRoot)
+    {
+        if(root==null)
+          return false;
+          if(check(root,subRoot))
+           return true;
+          return findRoot(root.left,subRoot)|| findRoot(root.right,subRoot);
+          
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+     return  findRoot(root,subRoot);
     }
 }
