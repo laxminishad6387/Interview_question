560b9a155f1a94cb9df7fd74881ff79bb492b967
laxmi nishad <laxminishad6387@gmail.com>
2025-02-01 11:04:47 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
-// dp problem (top-down approch)
 class Solution {
     public int climbStairs(int n) {
-        if(n>=0 && n<=3)
+       if(n<=3)
         return n;
-         int a=2;
-         int b=3;
-         int c=0;
-         for(int i=4; i<=n; i++)
-         {
-            c=a+b;
-            a=b;
-            b=c;
-         }
-         return  c;
+    int []dp=new int[n+1];
+   dp[0]=0;
+   dp [1]= 1;
+   dp[2]=2;
+   dp[3]=3;
+   for(int i=4; i<=n; i++)
+    dp[i]=dp[i-1]+dp[i-2];
+    
+     return dp[n];
+   
     }
 }
