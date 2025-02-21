876b263aa3382d8f137e56886031de64ffcea4f5
laxmi nishad <laxminishad6387@gmail.com>
2025-02-21 16:14:52 +0530

    Time: 21 ms (72.24%), Space: 47.5 MB (52.83%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.java
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.java
@@ -1,22 +1,20 @@
 
 class FindElements {
 private HashSet<Integer> set = new HashSet<>();
-    
+    // it is a constructor  of given class
     public FindElements(TreeNode root) {
-       setValue(root,0);
+       setValue(root,0);  // it is given that root always start from root node
     }
     void setValue(TreeNode root,int data)
     {
-       if(root==null)
+       if(root==null)   //base condition
         return ;
         root.val=data;
-        set.add(data);
-        setValue(root.left,2*data+1);
+        set.add(data);   //add  each node value  in hashset
+        setValue(root.left,2*data+1); 
         setValue(root.right,2*data+2);
     }
-
-    
-    public boolean find(int target) {
+   public boolean find(int target) {
         return set.contains(target);
     }
 }
