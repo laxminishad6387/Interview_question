806b055d1ebb3c7187f43accc50c39bb097effcf
laxmi nishad <laxminishad6387@gmail.com>
2025-03-11 13:35:23 +0530

    Time: 1 ms (99.85%), Space: 61.7 MB (51.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,18 @@
+// Kadane's Algorithm 
 class Solution {
     public int maxProfit(int[] nums) {
         int maxProfit=0;
-        int currentProfit=0;
+       
         int buy=nums[0];
         for(int i=1; i<nums.length; i++)
         {
-            if(nums[i]>buy)
-            {
-                currentProfit=nums[i]-buy;
-                maxProfit=Math.max(currentProfit, maxProfit);
-            }else if(nums[i]<buy)
+          if(nums[i]<buy)
             buy=nums[i];
+             else
+            {
+               int currentProfit=nums[i]-buy;
+                maxProfit=Math.max(currentProfit, maxProfit);
+            }
         }
         return maxProfit;
     }
