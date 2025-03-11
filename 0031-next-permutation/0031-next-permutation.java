22ce5cff1d0fcc6a8b6f49903e92c822ec7ab32a
laxmi nishad <laxminishad6387@gmail.com>
2025-03-11 17:05:59 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,36 +1,37 @@
 class Solution {
-    public void nextPermutation(int[] arr) {
-        int gola_index=-1;
-        for(int i=arr.length-1; i>0; i--)
+ 
+    public void nextPermutation(int[] nums) {
+        int golaIndex=-1;
+        int greaterIndex=-1;
+        for(int i=nums.length-1; i>0; i--)
         {
-            if(arr[i]>arr[i-1])
-            {
-                gola_index=i-1;
-                break;
+            if(nums[i-1]<nums[i]){
+            golaIndex=i-1;
+            break;
             }
         }
-        int greater_index=gola_index;
-        if(gola_index!=-1)
+        if(golaIndex!=-1)
         {
-            for(int i=arr.length-1; i>0; i--)
+            for(int i=nums.length-1; i>golaIndex;i--)
             {
-                if(arr[i]>arr[gola_index])
+                if(nums[i]>nums[golaIndex])
                 {
-                  greater_index=i;
-                  break;
+               greaterIndex=i;
+               break;
                 }
             }
-            int temp=arr[gola_index];
-            arr[gola_index]=arr[greater_index];
-            arr[greater_index]=temp;
+            int temp=nums[golaIndex];
+            nums[golaIndex]=nums[greaterIndex];
+            nums[greaterIndex]=temp;
         }
-        int j=0;
-        for(int i=gola_index+1; i<=(gola_index+arr.length)/2; i++)
-        {
-            int temp=arr[i];
-            arr[i]=arr[arr.length-1-j];
-            arr[arr.length-1-j]=temp;
-            j++;
+       
+        int left=golaIndex+1; int right=nums.length-1;
+        while(left<right){
+         int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
         }
     }
 }
