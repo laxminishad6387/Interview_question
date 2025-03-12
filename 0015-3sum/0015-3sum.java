62a40fa3f1fd5dd4079b15cb4ef5a17887d875ea
laxmi nishad <laxminishad6387@gmail.com>
2025-03-12 15:29:36 +0530

    Time: 30 ms (67.54%), Space: 51.1 MB (98.5%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        // Sort the array to make it easier to avoid duplicates and use two pointers
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for the current index i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            // Use two pointers to find the remaining two numbers
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Move left and right pointers to the next different values
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++; // Increase the sum by moving the left pointer to the right
-                } else {
-                    right--; // Decrease the sum by moving the right pointer to the left
+        List<List<Integer>>result=new ArrayList<>();
+        for(int i=0; i<nums.length-2; i++)
+        {
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int  j=i+1;
+            int k=nums.length-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                j++;
+                else if(sum>0)
+                k--;
+                else{
+                    List<Integer>temp=Arrays.asList(nums[i],nums[j],nums[k]);
+                    result.add(temp);
+                    j++; k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
             }
         }
-
         return result;
     }
 }
