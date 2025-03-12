a478b5569bbc3b8b7a18e89dfaaee94bb9a10629
laxmi nishad <laxminishad6387@gmail.com>
2025-03-12 15:45:00 +0530

    Time: 15 ms (88.68%), Space: 44.1 MB (51.11%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,46 +1,34 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] arr, int target) {
-        // store all ppsible result
-        List<List<Integer>> result=new ArrayList<>();
-        // sort the original array
-          Arrays.sort(arr);
-        // 4 sum problem 
-        for(int i=0; i<arr.length-3; i++)
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>>result=new ArrayList<>();
+        for(int i=0; i<nums.length-3; i++)
         {
-            if(i>0 && arr[i]==arr[i-1])
-            continue;
+            if(i!=0 && nums[i]==nums[i-1])continue;
             // 4 sum problem convert in 3 sum problem
-            // it is a 3 sum problem
-            for(int j=i+1; j<arr.length-2; j++)
+            for(int j=i+1; j<nums.length-2; j++)
             {
-                if(j>i+1 && arr[j]==arr[j-1])
-                  continue;
-                  int left=j+1;
-                  int right=arr.length-1;
-                  while(left<right)
-                  {
-                    long sum=(long)arr[i]+arr[j]+arr[left]+arr[right];
-                    // case 1 sum==target
-                    if(sum==target)
-                    {
-                        result.add(Arrays.asList(arr[i], arr[j],arr[left],arr[right]));
-                        while(left<right && arr[left]==arr[left+1]) //remove duplicate entry from the result
+                if(j!=i+1 && nums[j]==nums[j-1])continue;
+                int left=j+1; 
+                int right=nums.length-1;
+                while(left<right)
+                {
+                    long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum<target)
+                    left++;
+                    else if(sum>target)
+                    right--;
+                    else{
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
                         left++;
-                        while(left<right && arr[right]==arr[right-1])  //remove duplicate entry from the result
-                         right--;
-                         left++;   //apply each iteration
-                         right--;  // apply in each iteration
+                        right--;
+                        while(left<right && nums[left]==nums[left-1])left++;
+                        while(left<right && nums[right]==nums[right+1])right--;
                     }
-                    // case 2 sum is less than target
-                    else  if(sum<target)
-                      left++;
-                      else
-                       right--;
 
-                  }
+                }
             }
         }
         return result;
-
     }
 }
