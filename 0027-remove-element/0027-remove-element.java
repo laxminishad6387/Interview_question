e2a21a43afb5135aff43a42753a42cb406fd9bb2
laxmi nishad <laxminishad6387@gmail.com>
2025-07-28 11:17:28 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.08%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,16 @@
 class Solution {
-    public int removeElement(int[] arr, int val) {
-     //apply two pointer approch
-     int j=0;
-     for(int i=0; i<arr.length; i++)
-     {
-        if(arr[i]!=val)
-         arr[j++]=arr[i];
-     } 
-     return j;
+    public int removeElement(int[] nums, int val) {
+        int i=0; int j=0; 
+        while(i<nums.length)
+        {
+            if(nums[i]!=val)
+            {
+                nums[j]=nums[i];
+                i++; j++;
+            }
+            else i++;
+
+        }
+        return j;
     }
 }
