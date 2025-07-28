aa4c65db5fc81f0737cd79b98afdf779f7f7130d
laxmi nishad <laxminishad6387@gmail.com>
2025-07-28 11:58:48 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.31%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,37 @@
 class Solution {
- 
     public void nextPermutation(int[] nums) {
-        int golaIndex=-1;
-        int greaterIndex=-1;
-        for(int i=nums.length-1; i>0; i--)
+        int gola_index=-1;
+        int greater_index=-1;
+        for(int i=nums.length-1; i>0; i--)   //find first small value from the last
         {
-            if(nums[i-1]<nums[i]){
-            golaIndex=i-1;
-            break;
+            if(nums[i-1] <nums[i]){
+                gola_index=i-1;
+                break;
             }
         }
-        if(golaIndex!=-1)
+        if(gola_index!=-1)
         {
-            for(int i=nums.length-1; i>golaIndex;i--)
+             for(int i=nums.length-1; i>gola_index; i--)  //find just large value of gola_index value from the last index
+          {
+            if(nums[i]>nums[gola_index])
             {
-                if(nums[i]>nums[golaIndex])
-                {
-               greaterIndex=i;
-               break;
-                }
+                greater_index=i;
+                break;
             }
-            int temp=nums[golaIndex];
-            nums[golaIndex]=nums[greaterIndex];
-            nums[greaterIndex]=temp;
+          
+          }
+          int temp=nums[gola_index];
+            nums[gola_index]=nums[greater_index];
+            nums[greater_index]=temp;
         }
-       
-        int left=golaIndex+1; int right=nums.length-1;
-        while(left<right){
-         int temp=nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-            left++;
-            right--;
-        }
+        int left=gola_index+1; int right=nums.length-1;
+       while(left<right)
+       {
+        int temp=nums[left];
+        nums[left]=nums[right];
+        nums[right]=temp;
+        left++;
+        right--;
+       }
     }
 }
