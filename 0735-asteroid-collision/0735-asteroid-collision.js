378070d186c8b917f0e6b3f12f2a784e11db291d
laxmi nishad <laxminishad6387@gmail.com>
2026-02-06 11:55:36 +0530

    Time: 2 ms (90.89%), Space: 58.1 MB (23.57%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -5,29 +5,23 @@
 var asteroidCollision = function(asteroids) {
     let stack=[];
     for(let currast of asteroids){
-        console.log(currast);
+      
         let currastAlive=true;
         while(currastAlive && stack.length>0 && stack[stack.length-1]>0 && currast<0){
             let top=stack[stack.length-1];
         
-            if(Math.abs(top)<Math.abs(currast))
-            {
-                stack.pop();
-            }
+            if(Math.abs(top)<Math.abs(currast)) stack.pop();
+            
             else if(Math.abs(top)==Math.abs(currast))
             {
                 stack.pop();
                 currastAlive=false;
             }
-            else{
-                 currastAlive=false;
-            }
+            else currastAlive=false;
+            
         }
-        if( currastAlive)
-        {
-          
-            stack.push(currast);
-        }
+        if( currastAlive) stack.push(currast);
+        
     }
     return stack;
 };
